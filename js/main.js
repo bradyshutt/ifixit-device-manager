1c2b145bc084f7e05778b4a8766bc74c3e032c88
Brady <bradyshutt@gmail.com>
2016-08-16 11:57:51 -0700

    Basic working implementation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict'
 ;(function($) {
-
   /*
     'Samsung Galaxy S6',
     'Asus Laptop',
@@ -12,22 +11,71 @@
   */
 
   $(function pageReady() {
-    $('#search-submit').click(submitSearch)//.click()
+    $('#search-submit').click(submitSearch).click()
     $('#search-query').focus()
+
+    $('#results').click(deviceClicked)
+    $('#my-devices').click(myDevicesClicked)
 
     myDevices.loadDevices()
     myDevices.registerView('#my-devices')
     myDevices.updateViews()
   })
 
+
+  function myDevicesClicked(event) {
+    let target = $(event.target)
+    console.log(target)
+    if (target.text() === 'x') {
+      let device = target.parent().next().text()
+      console.log('clicked remove `x` for ', device)
+      myDevices.removeDevice(device) 
+    }
+  }
+
+  /* Catches click event on the container div as it
+   * bubbles up, NOT on the targeted 'device div' */
+  function deviceClicked(event) {
+    let target = $(event.target)
+    let deviceName = target.text()
+    if (target.children().length > 1) return
+    console.log('Clicked button:', deviceName)
+
+    if (myDevices.contains(deviceName)) {
+      myDevices.removeDevice(deviceName)
+      target.parent().removeClass('ownedDevice')
+    } else {
+      myDevices.addDevice(deviceName)
+      target.parent().addClass('ownedDevice')
+    }
+  }
+
   function submitSearch() {
     let query = $('#search-query').val()
     console.log('query:', query)
-    searchAPI(query).then((apiRes) => {
+    let results = searchAPI(query)
+    results.then((apiRes) => {
+      let results = apiRes.results.map(device => {
+        //let title = device['display_title']
+        let title = device['title']
+        /* Some "Device" pages have the term at the end?? 
+         * If so, remove it from these lists */
+        if (title.slice(-6).toLowerCase().trim() === 'repair')
+          title = title.slice(0, -6)
+        let el = $('<div>').html($('<span>').text(title))
+        if (myDevices.contains(title))
+          el.addClass('ownedDevice')
+        return el
+      })
       $('#results-meta').text('(' + apiRes.totalResults + ' results)')
-      let results = apiRes.results.map(device => $('<div>').text(device.title))
-      let numShowing = apiRes.moreResults ? 20 : apiRes.totalResults
       $('#results').html(results)
+      if (apiRes.totalResults > 20) {
+        $('<button>')
+          .text('Show More')
+          .addClass('showMoreButton')
+          .click(loadNextAPIResults)
+          .appendTo('#results')
+      }
     })
   }
 
@@ -37,10 +85,6 @@
         = 'https://www.ifixit.com/api/2.0/search/' 
         + encodeURIComponent(query) 
         + '?filter=category'
-      let urrl = 
-        'https://www.ifixit.com/api/2.0/wikis/CATEGORY?display=hierarchy'
-      let p = jQuery.getJSON(urrl)
-      //p.done((data) => { console.log(data) })
 
       jQuery.getJSON(url, resolve)
     })
--- js/myDevices.js
+++ js/myDevices.js
@@ -1,77 +1,91 @@
-/* myDevices.js *  * Keeps track of a list of devices that a user owns. * Relies on HTML5 LocalStorage. 
+/* myDevices.js 
+ *  
+ * Keeps track of a list of devices that a user owns. 
+ * Relies on HTML5 LocalStorage to save a list of 
+ * user-owned devices across page-reloads/browser restarts. 
  * */
 
 'use strict'
 ;(function(global, jQuery) {
 
-  let myDevices = []
+  let savedDevices = []
   let views = []
 
-  function loadDevices() {
-    console.log('Loading saved devices from local storage')
-    if (typeof Storage !== 'undefined') {
-      myDevices = myDevices || []
-      let loaded = JSON.parse(localStorage.getItem('myDevices')) || null
-      if (loaded)
-        loaded.forEach(el => { if (!myDevices[el]) myDevices.push(el) })
-    } else throw new Error('Local storage unavailable.')
-  }
-
-  function saveDevices() {
-    console.log('Saving devices to local storage')
-    if (typeof Storage !== 'undefined') {
-      localStorage.setItem('myDevices', JSON.stringify(myDevices))
-    } else throw new Error('Local storage unavailable.')
-  }
-
-  function registerView(view) { if (!views[view]) views.push(view) }
-
-  function updateViews() {
-    console.log('Updating view')
-    let elements = myDevices.map((el) => 
-        '<tr><td class="remove"><a>x</a></td>' + 
-        '<td class="name">' + el + '</td></tr>').join('')
-    views.forEach((view) => $(view).html(elements))
-  }
-
-  function addDevice(device) {
-    console.log('Adding', device, 'to saved devices')
-    myDevices.push(device)
-  }
-
-  function removeDevice(device) {
-    console.log(`Removing ${device} from collection`)
-    let idx
-    if ((idx = myDevices.indexOf(device)) > -1)
-      myDevices.splice(idx, 1)
-    else throw new Error('Attempted to remove', device, 'from collection.')
-  }
-
-  function logStorage() {
-    console.log('Local Storage Contents:')
-    console.log(JSON.parse(localStorage.getItem('myDevices')))
-  }
-
-  function logSaved() {
-    console.log('Saved Devices:')
-    console.log(myDevices)
-  }
-
-  function clearLocalStorage() {
-    console.log('Clearing local storage.')
-    localStorage.removeItem('myDevices')
-  }
-
   global.myDevices = {
-    loadDevices: loadDevices,
-    saveDevices: saveDevices,
-    registerView: registerView,
-    updateViews: updateViews,
-    addDevice: addDevice,
-    removeDevice: removeDevice,
-    logStorage: logStorage,
-    logSaved: logSaved,
-    clearLocalStorage: clearLocalStorage
+
+    loadDevices() {
+      console.log('Loading saved devices from local storage')
+      if (typeof Storage !== 'undefined') {
+        savedDevices = savedDevices || []
+        let loaded = JSON.parse(localStorage.getItem('myDevices')) || null
+        if (loaded)
+          loaded.forEach(el => { if (!savedDevices[el]) savedDevices.push(el) })
+      } else throw new Error('Local storage unavailable.')
+    }, 
+
+    saveDevices() {
+      console.log('Saving devices to local storage')
+      if (typeof Storage !== 'undefined') {
+        localStorage.setItem('myDevices', JSON.stringify(savedDevices))
+      } else throw new Error('Local storage unavailable.')
+    },
+
+    registerView(view) { 
+      if (!views[view]) views.push(view) 
+    },
+
+    updateViews() {
+      console.log('Updating view')
+      let elements = savedDevices.map((el) => 
+          '<tr><td class="remove"><a>x</a></td>' + 
+          '<td class="name"><span>' + el + '</span></td></tr>').join('')
+      views.forEach((view) => { $(view).html(elements) })
+    },
+
+    addDevice(device) {
+      device = device.trim()
+      console.log('Adding', device, 'to saved devices')
+      if (!savedDevices[device]) {
+        savedDevices.push(device)
+        myDevices.saveDevices()
+        myDevices.updateViews()
+      }
+    },
+
+    removeDevice(device) {
+      device = device.trim()
+      console.log(`Removing ${device} from collection`)
+      let idx
+      console.log('|' + device + '|')
+      console.log(myDevices.contains(device))
+      console.log(savedDevices)
+      if ((idx = savedDevices.indexOf(device)) > -1) {
+        savedDevices.splice(idx, 1)
+        myDevices.saveDevices()
+        myDevices.updateViews()
+      }
+      else throw new Error('Attempted to remove ' + device + ' from collection.')
+    },
+
+    contains(device) {
+      return (savedDevices.indexOf(device.trim()) > -1)
+    },
+
+
+    logStorage() {
+      console.log('Local Storage Contents:')
+      console.log(JSON.parse(localStorage.getItem('myDevices')))
+    },
+
+    logSaved() {
+      console.log('Saved Devices:')
+      console.log(savedDevices)
+    },
+
+    clearLocalStorage() {
+      console.log('Clearing local storage.')
+      localStorage.removeItem('myDevices')
+    }
   }
 
 })(window, jQuery)
