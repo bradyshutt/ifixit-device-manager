e8731c938c406850b307087eb18f08ce5b4bfb40
Brady <bradyshutt@gmail.com>
2016-08-17 13:28:49 -0700

    Refactored and cleaned up

--- js/myDevices.js
+++ js/myDevices.js
@@ -2,80 +2,73 @@
  *  
  * Keeps track of a list of devices that a user owns. 
  * Relies on HTML5 LocalStorage to save a list of 
- * user-owned devices across page-reloads/browser restarts. 
+ * user-owned devices across page-reloads & browser restarts. 
+ *
+ * Adds one interface object to global: `myDevices`
  * */
 
 'use strict'
-;(function(global, jQuery) {
+;(function(global) {
 
   let savedDevices = []
 
   global.myDevices = {
-
-    loadDevices() {
-      //console.log('Loading saved devices from local storage')
+    load() {
       if (typeof Storage !== 'undefined') {
         savedDevices = savedDevices || []
         let loaded = JSON.parse(localStorage.getItem('myDevices')) || null
-        if (loaded)
-          loaded.forEach(el => { if (!savedDevices[el]) savedDevices.push(el) })
+        if (loaded) {
+          loaded.forEach(el => { 
+            if (!savedDevices[el]) savedDevices.push(el) 
+          })
+        }
       } else throw new Error('Local storage unavailable.')
     }, 
 
-    saveDevices() {
-      //console.log('Saving devices to local storage')
-      if (typeof Storage !== 'undefined') {
-        localStorage.setItem('myDevices', JSON.stringify(savedDevices))
-      } else throw new Error('Local storage unavailable.')
-    },
-
-
-
-    addDevice(device) {
+    add(device) {
       device = device.trim()
-      //console.log('Adding', device, 'to saved devices')
       if (!savedDevices[device]) {
         savedDevices.push(device)
-        myDevices.saveDevices()
+        save()
       }
     },
 
-    removeDevice(device) {
+    remove(device) {
       device = device.trim()
-      //console.log(`Removing ${device} from collection`)
       let idx
       if ((idx = savedDevices.indexOf(device)) > -1) {
         savedDevices.splice(idx, 1)
-        myDevices.saveDevices()
+        save()
       }
       else throw new Error('Attempted to remove ' + device + ' from collection.')
     },
 
-    contains(device) {
-      return (savedDevices.indexOf(device.trim()) > -1)
-    },
-
-
-    logStorage() {
-      console.log('Local Storage Contents:')
-      console.log(JSON.parse(localStorage.getItem('myDevices')))
-    },
-
-    logSaved() {
-      console.log('Saved Devices:')
-      console.log(savedDevices)
-    },
-
-    clearLocalStorage() {
-      //console.log('Clearing local storage.')
-      localStorage.removeItem('myDevices')
-    },
-
-    getDevices() { return savedDevices }
+    contains(device) { return (savedDevices.indexOf(device.trim()) > -1) },
+    clearLocalStorage() { localStorage.removeItem('myDevices') },
+    getAll() { return savedDevices },
   }
 
-  myDevices.loadDevices()
 
-})(window, jQuery)
+  function save() {
+    if (typeof Storage !== 'undefined')
+      localStorage.setItem('myDevices', JSON.stringify(savedDevices))
+    else 
+      throw new Error('Local storage unavailable.')
+  }
+
+  function logStorage() {
+    console.log('Local Storage Contents:')
+    console.log(JSON.parse(localStorage.getItem('myDevices')))
+  }
+
+  function logSaved() {
+    console.log('Saved Devices:')
+    console.log(savedDevices)
+  }
+
+
+  /* Run this when the script is loading load */
+  myDevices.load()
+})(window)
 
 
